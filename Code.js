f46eeb4d2557ac3cdae938be68303290f748c719
Bill Murphy <bill559m@gmail.com>
2020-05-21 23:49:35 -0700

    seperated metric calculation from calling function

--- Code.js
+++ Code.js
@@ -1,20 +1,15 @@
-// const sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Raw Data'); 
-// const lr = sourceSheet.getLastRow();
-// const lc = souceSheet.getLastColumn();
-// const targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Calculated Data');
-
 const sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Raw Data'); 
 const targetSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Calculated Data');
 const lastRow = sourceSheet.getLastRow();
 const lastColumn = sourceSheet.getLastColumn();
 const searchRange = sourceSheet.getRange(2,1, lastRow, lastColumn);
-const rangeValues = searchRange.getValues(); // [2 - lastRow, 1 to lastColumn] matrix??? look at
+const rangeValues = searchRange.getValues(); // [2 - lastRow, 1 to lastColumn]
 
 const headerTitles = ['Facility', 'Number of Tickets', 'Total Hours', 'Average Time to Close','',
                       'Topic Category', 'Number of Tickets', 'Total Hours', 'Average Time to Close', '',
                       'ASR', 'Number of Tickets', 'Total Hours', 'Average Time to Close'];
 
-const __DEBUG = true; // see Logger output
+const __DEBUG = true; // Turn Logger On
 
 function onOpen(e) {
   targetSheet.getRange(2, 1, 50, 50).clearContent();
@@ -27,22 +22,55 @@ function onOpen(e) {
   addHeaders(headerTitles, 5);
 }
 
-function sortData() {
+/**
+* Add Headers and add a blank column as a seperator seperate column
+*/
+function addHeaders(headers){
+//  headers = headerTitles; // Uncomment for self-contained testing ONLY
+  for (let i = 0; i < headers.length; i++) {
+    const col = i + 1; 
+      targetSheet.getRange(1, col).setValue(headers[i]);
+  }
+  __DEBUG && Logger.log(headers);
+  __DEBUG && Logger.log('\n\n', rangeValues);
+}
+
+function calculateMetrics(col, key) {
+  const metricData = crunchNums(key);
+  targetSheet.getRange(2,col,metricData.length, 4).clearContent().setValues(metricData);
+  
+  __DEBUG && Logger.log(`\n${key}Data: \n`, metricData);
+  __DEBUG && Logger.log(`\n${key}Data.length: `, metricData.length);
+} // calculateMetrics
+
+function facilityTally() {
+  calculateMetrics(1, 'facility');
+} // faciltityTally
+
+function categoryTally() {
+ calculateMetrics(6, 'category'); 
+} // categoryTally
+
+function asrTally() {
+  calculateMetrics(11, 'asr');
+} // asrTally
+
+function getData() {
   let rowsData = [];
   for (let i = 0; i < rangeValues.length; i++){
-    let tempFacility = rangeValues[i][4];
-    let tempASR = rangeValues[i][5];
-    let tempCategory = rangeValues[i][12]; 
-    let tempStart = rangeValues[i][15];
-    let tempClose = rangeValues[i][17]; 
+    const tempFacility = rangeValues[i][4];
+    const tempASR = rangeValues[i][5];
+    const tempCategory = rangeValues[i][12]; 
+    const tempStart = rangeValues[i][15];
+    const tempClose = rangeValues[i][17]; 
     let tempTimeToClose = 'ticket is not closed';
     if (tempClose) {
-      let date1 = new Date(tempStart);
-      let date2 = new Date(tempClose);
-      let timeDiff = date2.getTime() - date1.getTime();
+      const date1 = new Date(tempStart);
+      const date2 = new Date(tempClose);
+      const timeDiff = date2.getTime() - date1.getTime();
       tempTimeToClose = timeDiff / (1000 * 3600);
     }
-    let row = {
+    const row = {
       facility: tempFacility,
       asr: tempASR,
       category: tempCategory,
@@ -50,131 +78,48 @@ function sortData() {
     };
     rowsData.push(row);
   }
+//  __DEBUG && Logger.log('rowsData: >>>> ', rowsData);
   return rowsData;
 }
 
-/**
-* Add Headers and add a blank column at seperate column
-*/
-function addHeaders(headers,seperate = 5){
-  headers = headerTitles; // Uncomment for self-contained testing ONLY
-  let length = headers.length;
-  for (let i = 0; i < length; i++) {
-    const col = i + 1;
-      targetSheet.getRange(1, col).setValue(headers[i]);
-  }
-  __DEBUG && Logger.log(headers);
-  __DEBUG && Logger.log('\n\n', rangeValues);
-}
-
-function asrTally() {
-  Logger.log('asrTally');
-}
-
-/*
-function facilityTally() {
-    var facilities = [];
+function crunchNums(key) {
+  let rowsData = getData();
+  rowsData.sort(compare(key));
+  __DEBUG && Logger.log('\n\nrowsData: \n', rowsData);
   
-  // clear tally column in target sheet
-  targetSheet.getRange(2, 1, 15, 2).clearContent();
+  let result = [];
+  let comparisonEl = rowsData[0];
+  let count = 0;
+  let time = 0;
+  let averageTime = 0;
+  __DEBUG && Logger.log('\n\nrtypeof comparisonEl.time: ', typeof comparisonEl.time);
   
-  // push content from all cells in column M into facilities array
-  for (let i = 2; i <lr+1; i++) {
-    var data = sourceSheet.getRange(i, 5).getValue();
-    facilities.push(data);
-  }
-  
-  // sort facilities array
-  facilities.sort();
-  
-  // initialize variables for counting unique elements in our array
-  let comparisonVal = facilities[0];
-  let cnt = 0;
-  let row = 2;
-  
-  // loop through the array
-  for (let i = 0; i <= facilities.length; i++){
-    // if the array element is not the same as the comparison value
-    // place it in a cell in column 4 with corresponding count in column 5
-    if (facilities[i] != comparisonVal) {
-       targetSheet.getRange(row,1).setValue(comparisonVal);
-       targetSheet.getRange(row,2).setValue(cnt);
-       row++; // move to the next row
-      comparisonVal = facilities[i]; // set the comparison value to the current array element
-      cnt = 1; // reset the count to one
-    } else { 
-      cnt++; //if the array element IS the same as the comparison value, simply increment the count
-    }
-  }  
-}
-
-
-function categoryTally() {
-  let topicsAndTimes = [];
-  targetSheet.getRange(2, 4, lr, 4).clearContent();
-  // loop through source sheet getting category, start time, and close time for each ticket
-  for (let i = 2; i < lr+1; i++){
-   let category = sourceSheet.getRange(i, 13).getValue();
-   let startTime = sourceSheet.getRange(i, 16).getValue();
-   let closeTime = sourceSheet.getRange(i, 18).getValue(); 
-   // if the ticket was closed, calculate the number of hours from open to close
-   if (closeTime) {
-     let date1 = new Date(startTime);
-     let date2 = new Date(closeTime);
-     let timeDiff = date2.getTime() - date1.getTime();
-     let hoursDiff = timeDiff / (1000 * 3600);
-     // push ticket category and time to close into 2 dimensional array
-     topicsAndTimes.push([category, hoursDiff]);
+  for (let i = 0; i < rowsData.length; i++){
+    let currentEl = rowsData[i];
+    if (currentEl[key] != comparisonEl[key]) {
+      if (count > 0){
+        averageTime = time/count;
+        result.push([comparisonEl[key], count, time.toFixed(2), averageTime.toFixed(2)]);
+      }
+      comparisonEl = currentEl;
+      count = 0;
+      time = 0;
+    } else {
+      count++;
+      const timeToClose = parseFloat(currentEl.timeToClose);
+      const currTimeToClose = Number.isNaN(timeToClose) ? 0 : timeToClose
+      time += parseFloat(currTimeToClose);
     }
   }
-  // sort the array by category
-  topicsAndTimes.sort();
-  
-  // initialize variables for counting unique elements in our array
-  let comparisonVal = topicsAndTimes[0][0];
-  let hrs = 0;
-  let cnt = 0;
-  let row = 2;
-  let numTopics = 0;
-  
-  // loop through the array
-  for (let i = 0; i < topicsAndTimes.length; i++){
-    // if the first element of each sub-array is not the same as the comparison value
-    // place it in a cell in column 4 with corresponding count in column 5 and time totals in column 6
-    if (topicsAndTimes[i][0] != comparisonVal) {
-       targetSheet.getRange(row,6).setValue(comparisonVal);
-       targetSheet.getRange(row,7).setValue(cnt);
-       targetSheet.getRange(row,8).setValue(hrs.toFixed(2));
-       row++; // move to the next row
-       comparisonVal = topicsAndTimes[i][0]; // set the comparison value to the first element current sub array
-       hrs = topicsAndTimes[i][1]; // set hours to second element of current sub array
-       cnt = 1; // reset the count to one
-       numTopics++; //tracking the total number of topics
-     }  else { 
-       hrs+= topicsAndTimes[i][1]; //if the array element IS the same as the comparison value, add to the total hours
-       cnt++; //and increment the count
-    }
-  }
-  
-  let totalTix = 0;
-  let totalTime = 0;
-  // for each row, use the number of tickets and total hours to calculate the average time to close
-  for (let i = 2; i < numTopics+2; i++){
-    let numTix = targetSheet.getRange(i, 7).getValue();
-    let time = targetSheet.getRange(i, 8).getValue();
-    let averageTime = (time / numTix)/24.0;
-    targetSheet.getRange(i, 9).setValue(averageTime);
-    // tracking the total number of tickets and total time for all tickets
-    totalTix += numTix;
-    totalTime += time;
-  }
-  
-  let totalAverageTime = (totalTime / totalTix) / 24.0;
-  // write information about totals to last row
-  targetSheet.getRange(numTopics+2, 6).setValue('Total');
-  targetSheet.getRange(numTopics+2, 7).setValue(totalTix);
-  targetSheet.getRange(numTopics+2, 8).setValue(totalTime);
-  targetSheet.getRange(numTopics+2, 9).setValue(totalAverageTime);  
+  averageTime = time/count;
+  result.push([comparisonEl[key], count, time.toFixed(2), averageTime.toFixed(2)]);
+  __DEBUG && Logger.log('\n\nresult: \n', result);
+  return result;
 }
 
-*/
+
+function compare(key) {
+  return function(a, b) {
+    return a[key] < b[key] ? -1 : 1;
+  }
+}
